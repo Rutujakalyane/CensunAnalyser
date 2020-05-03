39a5f468ebebe0fbcfc7108faf8af8e129e6d851
Rutujakalyane <kalyanerutuja7@gmail.com>
2020-05-03 15:00:28 +0530

    REMOVED:conficts happened

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -16,43 +16,40 @@ public class CensusAnalyser {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            List<IndiaCensusCSV> censusCSVList = csvBuilder.getCSVFileList(reader,IndiaCensusCSV.class);
-            return censusCSVList.size();
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvBuilder.getCSVFileIterator(reader,IndiaCensusCSV.class);
+            return getCount(censusCSVIterator);
         } catch (IOException e) {
-            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-        }  catch (CSVBuilderException e) {
-            throw new CensusAnalyserException(e.getMessage(),e.type.name());
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }catch (RuntimeException e) {
             throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.WRONG_DELIMITER);
-        } catch (Exception e) {
-            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.WRONG_HEADER_PROBLEM);
+        } catch(Exception e){
+            throw  new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.WRONG_HEADER_PROBLEM);
         }
     }
-
     public int loadIndiaStateCodeData(String csvFilePath) throws CensusAnalyserException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            List<CSVStates> censusCSVList = csvBuilder.getCSVFileList(reader,CSVStates.class);
-            return censusCSVList.size();
+            Iterator<CSVStates> censusCSVIterator = csvBuilder.getCSVFileIterator(reader,CSVStates.class);
+            return getCount(censusCSVIterator);
         } catch (IOException e) {
-            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-        }catch (CSVBuilderException e) {
-            throw new CensusAnalyserException(e.getMessage(),e.type.name());
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         } catch (RuntimeException e) {
             throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.WRONG_DELIMITER);
-        } catch (Exception e) {
-            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.WRONG_HEADER_PROBLEM);
+        }catch (Exception e){
+            throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.WRONG_HEADER_PROBLEM);
         }
+
     }
 
     public <E> int getCount(Iterator<E> censusCSVIterator) {
         Iterable<E> csvIterable = () -> censusCSVIterator;
         return (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
     }
-    public String getStateWiseSortedCensusData(String indiaStateCodeCsvFilePath) {
-        return null;
-    }
+
 }
 
 
--- src/main/java/censusanalyser/ICSVBuilder.java
+++ src/main/java/censusanalyser/ICSVBuilder.java
@@ -3,8 +3,7 @@ import java.io.Reader;
 import java.util.Iterator;
 import java.util.List;
 
-public interface ICSVBuilder <E>{
-    Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvStatesClass);
+public interface ICSVBuilder {
+    <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvStatesClass);
 
-    List<E> getCSVFileList(Reader reader, Class<E> indiaCensusCSVClass);
 }
